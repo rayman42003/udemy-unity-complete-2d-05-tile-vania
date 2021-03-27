399b02b81f2f0f975f94223fa3b95c2dca1080b1
Richmond Chang <changrichmond@gmail.com>
2021-03-26 23:23:56 -0700

    [Lesson 191] - Add jump to player:     -  Add jump input key mapping, and jump method to movable     -  Fix player animation so run animation only plays when player is moving left and right and button is pressed

--- Assets/Scripts/Movable.cs
+++ Assets/Scripts/Movable.cs
@@ -4,7 +4,10 @@ using UnityEngine.InputSystem;
 public class Movable : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed = 50f;
+    private float moveSpeed = 5f;
+
+    [SerializeField]
+    private float jumpSpeed = 50f;
 
     private float currXSpeed = 0f;
     private Rigidbody2D _rigidbody;
@@ -19,5 +22,12 @@ public class Movable : MonoBehaviour
 
     public void setMoveSpeed(InputAction.CallbackContext ctx) {
         currXSpeed = ctx.ReadValue<Vector2>().x;
+    }
+
+    public void jump(InputAction.CallbackContext ctx) {
+        if (ctx.started) {
+            Vector2 verticalSpeed = new Vector2(0f, jumpSpeed);
+            _rigidbody.velocity += verticalSpeed;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     private static readonly float FLIP_EPSILON = 0.0001f;
     private Animator animator;
     private Rigidbody2D _rigidbody;
+    private bool moveKeyPressed = false;
 
     private void Start() {
         animator = GetComponent<Animator>();
@@ -13,6 +15,14 @@ public class Player : MonoBehaviour
 
     private void Update() {
         float xVelocity = _rigidbody.velocity.x;
-        animator.SetBool("Running", Mathf.Abs(xVelocity) > FLIP_EPSILON);
+        animator.SetBool("Running", Mathf.Abs(xVelocity) > FLIP_EPSILON && moveKeyPressed);
+    }
+
+    public void onMoveKeyPressed(InputAction.CallbackContext ctx) {
+        if (ctx.started) {
+            moveKeyPressed = true;
+        } else if (ctx.canceled) {
+            moveKeyPressed = false;
+        }
     }
 }
